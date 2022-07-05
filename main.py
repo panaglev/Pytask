740cd060f34d4d752f9898badeb2ce239424b6cb
panaglev <>
2022-07-05 19:01:22 +0700

    It works separately but not together, still fixing this

--- main.py
+++ main.py
@@ -4,11 +4,10 @@
 #Night and light themes
 #To be continued
 
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLineEdit, QMessageBox, QListWidget, QGridLayout
+from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow, QPushButton, QLineEdit,
+			QMessageBox, QListWidget, QGridLayout, QSplitter, QGridLayout)
 from PyQt5.QtCore import QSize
 import sys
-
-all_tasks = {}
 
 class MainWindow(QMainWindow):
 	def __init__(self):
@@ -18,13 +17,14 @@ class MainWindow(QMainWindow):
 	def button_addtask_clicked(self):
 		TitleValue = self.task_title.text()
 		DescriptionValue = self.task_description.text()
-		all_tasks[TitleValue] = DescriptionValue
-		#print(TitleValue, " ", DescriptionValue)
-		#print(all_tasks[TitleValue])
-		#create windows with successfull message
+
+		with open("tasks.txt", "a") as file_with_tasks:
+			file_with_tasks.write(TitleValue + " " + DescriptionValue + "\n")
+			file_with_tasks.close()
 
 		QMessageBox.question(self, 'Message', "Task saved!", QMessageBox.Ok, QMessageBox.Ok)
 
+		#Redesigned button
 		#addtaskBtn = QMessageBox()
 		#addtaskBtn.setIcon(QMessageBox.Information)
 		#addtaskBtn.setText("Your task has been successfully added")
@@ -34,7 +34,7 @@ class MainWindow(QMainWindow):
 	def initUI(self):
 		self.setWindowTitle("Pytask")
 		self.setGeometry(0, 0, 1100, 700)
-		#self.setFixedSize(QSize(1100, 700))
+
 		#Set checking if window size less than *some_value* and if bigger *some_value* to display
 		#Different interfaces
 
@@ -54,9 +54,34 @@ class MainWindow(QMainWindow):
 		self.task_description.resize(280, 40) 
 
 
+class ListWidget(QListWidget):
+	LOCAL_STORAGE = [] #At first saving to local storage and than dispaly in widget
+
+	def __init__(self):
+		super().__init__()
+		self.fillLocalStorage()
+		self.addItems(self.LOCAL_STORAGE)
+		self.initUI()
+
+	def fillLocalStorage(self):
+		with open("tasks.txt", "r") as file_with_tasks:
+			counter = 0 
+			try:
+				for line in file_with_tasks:
+					self.LOCAL_STORAGE.append(line) 
+				#	counter += 1
+				#	print(line)
+			except:
+				pass
+
+	def initUI(self):
+		self.resize(300, 300)
+		#self.setStyleSheet('font-size: 35px;')
+
 def main():
 	app = QApplication([])
-	main_window = MainWindow() 
+	#main_window = MainWindow() 
+	main_window = ListWidget()
 	main_window.show()
 	sys.exit(app.exec_())
 
